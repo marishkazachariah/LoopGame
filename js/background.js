e08f1c5090fd7328d9cd29223600920ef9082b1f
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-08-24 15:04:00 +0200

    removed unused variables

--- js/background.js
+++ js/background.js
@@ -1,13 +1,9 @@
 class Background {
-    constructor() {
-        this.image;
-        this.x = 200;
-        this.y = 200;
-        this.width = 400;
-        this.height = 400;
-    }
+  constructor() {
+    this.image;
+  }
 
-    draw() {
-        image(this.image, 0, 0, 400, 400);
-    }
+  draw() {
+    image(this.image, 0, 0, 400, 400);
+  }
 }
