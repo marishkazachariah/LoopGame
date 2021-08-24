b97273296e38d3041e0d3492f1362acb47612632
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-08-24 15:03:10 +0200

    remove bed obstacle and increased size of bedside area obstacle

--- js/obstacles.js
+++ js/obstacles.js
@@ -4,14 +4,14 @@ class Obstacles {
     fill(0, 0, 255, 60);
     noStroke();
     rect(40, 111, 44, 62);
-    //collider for bed
-    fill(0, 0, 255, 80);
-    noStroke();
-    rect(127, 89, 120, 145);
+    // collider for bed
+    // fill(0, 0, 255, 80);
+    // noStroke();
+    // rect(127, 89, 120, 145);
     // collider for beside area
     fill(0, 0, 255, 60);
     noStroke();
-    rect(248, 100, 38, 118);
+    rect(235, 100, 48, 118);
     // collider for dresser
     fill(0, 0, 255, 60);
     noStroke();
