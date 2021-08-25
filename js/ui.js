69d807b6de66053f7e69f1c243479cabb04090e9
Marishka Zachariah <marishka.zachariah@gmail.com>
2021-08-25 16:59:01 +0200

    implement instructions function that receives a string argument so instructions on how to proceed pop up

--- js/ui.js
+++ js/ui.js
@@ -13,21 +13,28 @@ class Ui {
 
     startScreen() {
         image(game.startScreenImage, this.x, this.y, this.width, this.height);
-        textSize(14);
-        textFont('VT323'); // Referencing Google Font via font name
+        textSize(this.textSize);
+        textFont(this.textFont); 
+        fill(this.textFill);
         text("Start Game", 100, 135);
     }
 
     gameOver() {
         image(game.gameOverImage, this.x, this.y, this.width, this.height);
         textSize(this.textSize);
-        textFont(this.textFont); // Referencing Google Font via font name
+        textFont(this.textFont); 
         fill(this.textFill);
         text("Game Over", this.textX, this.textY);
+        this.instructions('Press R to Restart');
     }
 
     win() {
         image(game.winImage, this.x, this.y, this.width, this.height);
+        textSize(this.textSize);
+        textFont(this.textFont); 
+        fill(this.textFill);
+        text("You Win!", this.textX, this.textY);
+        this.instructions('Press R to Replay Game');
     } 
     
     // change to keyboard instructions?
@@ -37,5 +44,13 @@ class Ui {
         let button = createImg(game.buttonImage, "button");
         button.position(150, 235);
         button.mousePressed(this.restartGame);
+        this.instructions('Press Enter to Start Game');
+    }
+
+    instructions(instructions) {;
+        textSize(20);
+        textFont(this.textFont); 
+        fill(this.textFill);
+        text(instructions, 125, 230);
     }
 }
